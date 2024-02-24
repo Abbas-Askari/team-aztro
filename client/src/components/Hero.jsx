5989ba2bb81d53e63a8d656388a8e130a5b6c3e2
Zain Ali <zali.bscs22seecs@seecs.edu.pk>
2024-02-24 15:27:33 +0000

    Changes on Home

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,82 +1,42 @@
-import Button from './Button'
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import beachVid from '../assets/beachVid.mp4';
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      <div className="hero-map" />
-
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <img 
-          src="/camp.svg"
-          alt="camp"
-          width={50}
-          height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
-        />
-        <h1 className="bold-52 lg:bold-88">Aztro Camp Area</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
-        </p>
-
-        <div className="my-11 flex flex-wrap gap-5">
-          <div className="flex items-center gap-2">
-            {Array(5).fill(1).map((_, index) => (
-              <img 
-                src="/star.svg"
-                key={index}
-                alt="star"
-                width={24}
-                height={24}
-              />
-            ))}
+    <div className='w-full h-screen relative'>
+      <video
+        className='w-full h-full object-cover'
+        src={beachVid}
+        autoPlay
+        loop
+        muted
+      />
+      <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
+      <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+        <h1>First Class Travel</h1>
+        <h2 className='py-4'>Top 1% Locations Worldwide</h2>
+        <form
+          className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
+          rounded-md text-black bg-gray-100/90'
+        >
+          <div>
+            <input
+              className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
+                  '
+              type='text'
+              placeholder='Search Destinations'
+            />
           </div>
-
-          <p className="bold-16 lg:bold-20 text-blue-70">
-            198k
-            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
-          </p>
-        </div>
-
-        <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button 
-            type="button" 
-            title="Book Now" 
-            variant="btn_green" 
-          />
-          <Button 
-            type="button" 
-            title="How we work?" 
-            icon="/play.svg"
-            variant="btn_white_text" 
-          />
-        </div>
+          <div>
+            <button>
+              <AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}  />
+            </button>
+          </div>
+        </form>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-
-           <div className="flex flex-col">
-            <div className="flexBetween">
-              <p className="regular-16 text-gray-20">Location</p>
-              <img src="/close.svg" alt="close" width={24} height={24} />
-            </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
-          </div>
-
-          <div className="flexBetween">
-            <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
-            </div>
-            <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Elevation</p>
-              <p className="bold-20 text-white">2.040 km</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
-
-export default Hero
+export default Hero;
