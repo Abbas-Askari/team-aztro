d74bdd7fef1203c0a6952431a1bbc37c151fb00c
MAskari22 <maskari.bscs22seecs@seecs.edu.pk>
2024-02-25 03:16:37 +0500

    events

--- client/src/components/trips/TripsList.jsx
+++ client/src/components/trips/TripsList.jsx
@@ -2,9 +2,33 @@ import { useSelector } from "react-redux";
 import { dataStore } from "../../features/dataSlice";
 import LoadingSkeleton from "../skeletons/LoadingSkeleton";
 import SingleTrip from "./SingleTrip";
+import { useEffect, useState } from "react";
 
 const TripsList = () => {
-  const { currentDataItems, loading } = useSelector(dataStore);
+  let { currentDataItems, loading } = useSelector(dataStore);
+  const [realData, setRealDate] = useState([]);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_BACKEND + "/trips")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRealDate(data.trips);
+      });
+  });
+
+  if (realData.length > 0) {
+    return loading ? (
+      <LoadingSkeleton basis="basis-[18rem]" />
+    ) : (
+      <div className="flex flex-wrap gap-4 mt-10">
+        {realData?.map((trip) => (
+          <SingleTrip {...trip} key={trip?.id} />
+        ))}
+      </div>
+    );
+  }
+
   return loading ? (
     <LoadingSkeleton basis="basis-[18rem]" />
   ) : (
