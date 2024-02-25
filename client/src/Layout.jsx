032598c848b467361be8591398f1f48ddf83219c
MAskari22 <maskari.bscs22seecs@seecs.edu.pk>
2024-02-25 09:15:59 +0500

    before

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,25 +1,23 @@
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Footer from "./components/common/Footer";
 import Navbar from "./components/common/Navbar";
-import { Outlet } from 'react-router-dom';
-import { Chat } from './components/Chat';
-import { io } from "socket.io-client"
+import { Outlet } from "react-router-dom";
+import { Chat } from "./components/Chat";
+import { io } from "socket.io-client";
 
-const serverURL = "http://localhost:4000"
+const serverURL = "http://localhost:4000";
 
 const Layout = ({ children }) => {
-
-  const [messages, setMessages] = useState([])
-  const [collapsed, setCollapsed] = useState(false)
-  const [socket, setSocket] = useState(io(serverURL))
+  const [messages, setMessages] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
+  // const [socket, setSocket] = useState(io(serverURL));
 
   return (
     <div>
       <Navbar />
-      <div className='relative'>
+      <div className="relative">
         <Outlet />
-        {socket && <Chat socket={socket} collapsed={collapsed} setCollapsed={setCollapsed} messages={messages} setMessages={setMessages}/>}
+        {/* {socket && <Chat socket={socket} collapsed={collapsed} setCollapsed={setCollapsed} messages={messages} setMessages={setMessages}/>} */}
         {/* {children} */}
       </div>
       <Footer />
